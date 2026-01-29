7f9758393b9fbbd3f32fe154906f5b2afa273c13
Andy <Andydeuter2021@gmail.com>
2026-01-28 21:14:59 -0600

    crecion de clases

---
+++ datos/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datos
+{
+    public class Conexion
+    {
+        private static string cadena =
+            "Data Source=DEVELOPER;Initial Catalog=LoginDB;Integrated Security=True";
+
+        public static SqlConnection obtenerConexion()
+        {
+            return new SqlConnection(cadena);
+        }
+
+    }
+}
